e2c53543489392192a5a14229410c39d2c9f503c
WebDev Jones <webdeveloper.jones@gmail.com>
2020-10-05 12:57:55 -0400

    remove console logs

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -10,37 +10,32 @@ if (process.env.NODE_ENV === 'production') {
     BaseGetUrl = '/api/ads/'
     BaseCurrUrl = '/api/ads/current/'
     BaseDeleteUrl = '/api/ads/delete/'
-    BasePassCheck = 'http://localhost:3001/api/passcheck/'
+    BasePassCheck = '/api/passcheck/'
 }
 
 const checkPass = async pass => {
     const response = await axios.post(`${BasePassCheck}${pass}`)
-    console.log(response.data)
     return(response.data.accept)
 }
 
 const addAd = async (Data, type) => {
-    console.log("Posting data: ", Data)
     const response = await axios.post(`${BaseAddUrl}${type}`, Data, {
         headers: {
             'Accept': 'multipart/form-data',
             'Content-Type': 'multipart/form-data',
         }
     })
-    console.log("Success")
     return response
 }
 
 const getAll = async () => {
     const response = await axios.get(BaseGetUrl)
-    console.log(response)
     return response.status === 200 ? response.data : 0
 
 }
 
 const setCurr = async id => {
     const response = await axios.post(`${BaseCurrUrl}${id}`)
-    console.log('setcurr', response)
     return response.status === 200 ? response : 0
 }
 
